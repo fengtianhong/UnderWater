b8f81cfa7774cb8518402a58cc2e517ec1b34490
foo <ks369108@gmail.com>
2021-05-20 12:09:32 +0800

    Add QaVO & change newsDAO_interface name.

--- UnderWater/src/com/model/newsDAO_interface.java
+++ UnderWater/src/com/model/newsDAO_interface.java
@@ -2,7 +2,7 @@ package com.model;
 
 import java.util.List;
 
-public interface NewsDAO_interface {
+public interface newsDAO_interface {
     public void insert(NewsVO newsVO);
     public void update(NewsVO newsVO);
     public void delete(NewsVO newsVO);
