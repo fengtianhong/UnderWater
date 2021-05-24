a598e359c475d1e60d0c4ba8774477ca9e3ccaf3
foo <ks369108@gmail.com>
2021-05-25 00:24:30 +0800

    Add CollectionsDAO.

--- UnderWater/src/com/collections/model/CollectionsDAO_interface.java
+++ UnderWater/src/com/collections/model/CollectionsDAO_interface.java
@@ -3,9 +3,9 @@ package com.collections.model;
 import java.util.List;
 
 public interface CollectionsDAO_interface {
-	public void insert();
-	public void update();
-	public void delete();
-	public List<CollectionsVO> findByUserID(Integer userID);	// // FK need imports 顯示會員收藏的套裝行程
+	public void insert(CollectionsVO collectionsVO);
+	public void update();	// no need
+	public void delete(CollectionsVO collectionsVO);
+	public List<Integer> findByUserID(Integer userID);	// // FK need imports 顯示會員收藏的套裝行程
 	
 }
